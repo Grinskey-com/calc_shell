6f1d53ab9a7c5166c02fe2025cf2eda95232aea4
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-09-17 14:28:27 +1000

    working token system

--- main.c
+++ main.c
@@ -1,11 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 int main()
 {
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
+	char *token;
+	int num1, num2, op, i;	
+	char *token_arr[1024];
+	int n; 
 
 	while(1)
 	{
@@ -13,13 +18,30 @@ int main()
 		read = getline(&line, &len, stdin);
 
 		if (read != -1)
-			printf("you entered: %s\n", line);
+		{
+			token = strtok(line, " ");
+
+			while (token != NULL)
+			{	
+				token_arr[i] = token;
+				i++;
+				token = strtok(NULL, " ");
+			}
+
+			num1 = atoi(token_arr[0]);
+			op = atoi(token_arr[1]);
+			num2 = atoi(token_arr[2]);
+			printf("num1:%d op:%d num2:%d\n",num1 ,op ,num2); 
+		}
 		else
 		{
-			printf("something went wrong, please try again\n");
+			printf("ERROR\n");
+			free(line);
 			break;
 		}
 	}
 	free(line);
 	return(0);
 }
+
+
