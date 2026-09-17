5b037918ee10b1377b79a096b32a066c44063ec1
grinskey-com <4zw5nh5f4k@privaterelay.appleid.com>
2026-09-17 15:29:43 +1000

    ops table, ops func, h file and changed op back to char

--- main.c
+++ main.c
@@ -1,6 +1,10 @@
+#include "main.h"
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+
+int (*get_op_func(char *s))(int, int);
 
 int main()
 {
@@ -8,9 +12,11 @@ int main()
 	size_t len = 0;
 	ssize_t read;
 	char *token;
-	int num1, num2, op, i;	
+	int num1, num2, i;	
 	char *token_arr[1024];
-	int n; 
+	int n;
+	int (*func)(int, int);
+	char *op;
 
 	while(1)
 	{
@@ -29,9 +35,20 @@ int main()
 			}
 
 			num1 = atoi(token_arr[0]);
-			op = atoi(token_arr[1]);
+			op = token_arr[1];
 			num2 = atoi(token_arr[2]);
-			printf("num1:%d op:%d num2:%d\n",num1 ,op ,num2); 
+		//	printf("num1:%d op:%s num2:%d\n",num1 ,op ,num2);
+
+			func = get_op_func(op);
+
+			if (func == NULL)
+			{
+				printf("ERROR\n");
+				exit(1);
+			}
+
+			printf("%d\n", func(num1, num2));
+
 		}
 		else
 		{
